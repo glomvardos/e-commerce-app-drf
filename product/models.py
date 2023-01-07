88322eb4e3a739a3dc7605d8ff214b82e555c085
glomvardos <glomvardos@gmail.com>
2023-01-07 15:25:08 +0200

    Added many to many relation between categoried and products

--- product/models.py
+++ product/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from category.models import Category
 
 
 class Product(models.Model):
     name = models.CharField(max_length=60)
-    category = models.CharField(max_length=60)
     price = models.FloatField()
     quantity = models.IntegerField()
     description = models.TextField(max_length=160)
     discount = models.IntegerField(default=0, blank=True)
+    categories = models.ManyToManyField(Category, related_name='products')
 
-
+  
